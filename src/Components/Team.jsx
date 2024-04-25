673e5e1d41c30c293a94b33014db23e2d570efb5
Blackdevil-03 <vasan77492@gmail.com>
2024-04-25 13:00:17 +0530

    teams changes

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Team = () => {
   return (
-    <div>
-      this is team
+    <>
+    <div className="p-4">
+      <figure className="md:flex max-w-5xl bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto object-cover" src="https://images.unsplash.com/photo-1495716868937-273203d5bb0c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxsYWR5fGVufDB8MHx8fDE2OTQxNzI4MTV8MA&ixlib=rb-4.0.3&q=80&w=1080" alt="Tailwind CSS" width="384" height="512" />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium dark:text-slate-100">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              Sarah Dayan
+            </div>
+            <div className="text-slate-700 dark:text-slate-400">
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
     </div>
+  </>
   )
 }
 
