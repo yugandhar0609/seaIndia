3582eb5150eabf0bbd351daa7a52eee807200ab1
manojkumar033010 <manokutty5650@gmail.com>
2024-04-28 13:19:44 +0530

    changed

---
+++ src/Components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import img1 from "../assets/ship2.jpg"
+
+const Services = () => {
+  return (
+    <div>
+      <div className='sm:'>
+      <img src={img1} alt="ship" />
+      </div>
+    </div>
+  )
+}
+
+export default Services
