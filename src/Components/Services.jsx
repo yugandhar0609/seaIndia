3131f082feefd8b76445b8bd193018609e4c4392
manojkumar033010 <manokutty5650@gmail.com>
2024-04-28 13:22:02 +0530

    components created

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,26 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-import img1 from "../assets/ship2.jpg"
+import React from 'react'
 
 const Services = () => {
   return (
     <div>
-      <div className='sm:'>
-      <img src={img1} alt="ship" />
-      </div>
+      <h1>Bye!!</h1>
     </div>
   )
 }
 
 export default Services
-=======
-import React from 'react'
-
-const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
-
-export default Services
->>>>>>> 4820c506d1a68f0d7f15bfa207b61eed4a07b61e
