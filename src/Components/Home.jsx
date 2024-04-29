f6ded2c3fd744ea15c8ae60461b17b6c3a50a38f
Cirilando <cirilando2244@gmail.com>
2024-04-29 20:09:34 +0530

    added some contents in home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,10 @@
 import img1 from "../assets/Bg.jpeg";
 import image from "../assets/choose.jpeg";
+import why from "../assets/why.jpeg";
+import figma from "../assets/figma.png";
 import "../Components/css/Home.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { FaEarthAsia } from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -19,7 +22,53 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <div className="mt-10 container mx-auto px-4 sm:px-6 lg:px-8">
+
+      <div>
+        <p className=" md:mt-4 text-center text-3xl font-extrabold text-orange-500">
+          Welcome to SEAINDIA Freight System Private Limited!
+        </p>
+      </div>
+      <div className="md:flex">
+        <div className="md:mt-5 md:flex-wrap ">
+          <div className=" md:ml-10 text-6xl font-bold font-serif">
+            <p>Solutions For Your</p>
+            <p>Business Need</p>
+          </div>
+          {/* <div>
+          <FaEarthAsia className="text-2xl"/>
+          </div> */}
+        </div>
+        <div class="max-w-lg mx-auto  rounded-xl overflow-hidden shadow-md">
+          <div class="p-4">
+            <div class="text-lg md:mr-10">
+              <p class="text-gray-700">
+                We are your strong fortress for all your shipping requirements.
+                Our experience and knowledge of the shipping industry give you
+                and your cargo a competitive edge in your business. Our prayer
+                for our valuable customers and us..
+              </p>
+              <p class="text-gray-700 mt-4">
+                May the Lord, the God of your fathers, increase you a thousand
+                times and bless you as he has promised - Deuteronomy 1:11
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="text-lg md:mr-10">
+          <p className="">
+            We are your strong fortress for all your shipping requirements. Our
+            experience and knowledge of the shipping industry gives you and your
+            cargo a competitive edge in your business. Our prayer for our
+            valuable customers and us..
+          </p>
+          <p>
+            May the Lord, the God of your fathers, increase you a thousand times
+            and bless you as he has promised - Deuteronomy 1:11
+          </p>
+        </div> */}
+
+      {/* <div className="mt-10 container mx-auto  px-8 lg:px-8">
         <p className=" title text-center text-3xl font-extrabold text-orange-500">
           Welcome to SEAINDIA Freight System Private Limited!
         </p>
@@ -57,12 +106,13 @@ const Home = () => {
           commitment to your success, we look forward to continuing our journey
           together, guided by faith and fueled by dedication.
         </p>
-      </div>
+      </div> */}
+
       <div className="relative">
         <img
           src={image}
           alt="background"
-          className="w-full md:h-[400px] object-cover opacity-80"
+          className=" mt-10 w-full md:h-[400px] object-cover opacity-80"
         />
         <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center">
           <p className="text-lg md:text-xl lg:text-2xl font-extrabold text-black">
@@ -75,11 +125,253 @@ const Home = () => {
             For Book Your Shipping From Any Country
           </p>
 
-          <Link to ="/contact"><button className="mt-3 bg-orange-500 font-bold text-white text-lg rounded-3xl p-2 md:mt-10 hover:bg-orange-600 hover:border hover:border-white">
-            Contact With Us
-          </button></Link>
+          <Link to="/contact">
+            <button className="mt-3 bg-orange-500 font-bold text-white text-lg rounded-3xl p-2 md:mt-10 hover:bg-orange-600 hover:border hover:border-white">
+              Contact With Us
+            </button>
+          </Link>
         </div>
       </div>
+
+      <div className=" flex flex-wrap mt-5 mx- grid md:grid-cols-3 md:gap-16 md:mt-16">
+        <div>
+          <img
+            src={why}
+            alt="images"
+            className=" rounded-xl  md:ml-40 md:w-[460px] md:h-[290px]"
+          />
+        </div>
+        <div>
+          <p className=" mt-4 md:ml-28 text-xl font-semibold">
+            Why Choose Us ?
+          </p>
+          <p className=" mt-4 md:ml-28 md:mt-3">
+            Our extensive global network ensures seamless transport, while our
+            tailored solutions cater to your unique requirements. Count on us
+            for on-time delivery, secure handling, and open communication
+          </p>
+          <p className=" mt-4 md:ml-28 md:mt-3">
+            Embracing innovation, we utilize technology for optimized logistics.
+            Choose SeaIndia Freight Systems as your trusted partner for
+            dependable success.
+          </p>
+        </div>
+        <div>
+          <img
+            src={figma}
+            alt="services"
+            className=" mt-4 md:w-64 md:h-[290px]"
+          />
+        </div>
+      </div>
+
+      <section className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+          <h2 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Read trusted reviews from our customers
+          </h2>
+
+          <div className="mt-8 grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
+            <blockquote className="rounded-lg transform hover:scale-110 transition-transform duration-500 ease-in-out hover:border border-orange-500 p-6 shadow-xl sm:p-8">
+              <div className="flex items-center gap-4">
+                <img
+                  alt=""
+                  src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                  className="size-14 rounded-full object-cover"
+                />
+
+                <div>
+                  <div className="flex justify-center gap-0.5 text-green-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  </div>
+
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Paul Starr
+                  </p>
+                </div>
+              </div>
+
+              <p className="mt-4 text-gray-700">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
+                sit rerum incidunt, a consequuntur recusandae ab saepe illo est
+                quia obcaecati neque quibusdam eius accusamus error officiis
+                atque voluptates magnam!
+              </p>
+            </blockquote>
+
+            <blockquote className="rounded-lg transform hover:scale-110 transition-transform duration-500 ease-in-out hover:border border-orange-500 p-6 shadow-xl sm:p-8">
+              <div className="flex items-center gap-4">
+                <img
+                  alt=""
+                  src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                  className="size-14 rounded-full object-cover"
+                />
+
+                <div>
+                  <div className="flex justify-center gap-0.5 text-green-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  </div>
+
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Paul Starr
+                  </p>
+                </div>
+              </div>
+
+              <p className="mt-4 text-gray-700">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
+                sit rerum incidunt, a consequuntur recusandae ab saepe illo est
+                quia obcaecati neque quibusdam eius accusamus error officiis
+                atque voluptates magnam!
+              </p>
+            </blockquote>
+
+            <blockquote className="rounded-lg transform hover:scale-110 transition-transform duration-500 ease-in-out hover:border border-orange-500  p-6 shadow-xl sm:p-8">
+              <div className="flex items-center gap-4">
+                <img
+                  alt=""
+                  src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                  className="size-14 rounded-full object-cover"
+                />
+
+                <div>
+                  <div className="flex justify-center gap-0.5 text-green-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="orange"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  </div>
+
+                  <p className="mt-0.5 text-lg font-medium text-gray-900">
+                    Paul Starr
+                  </p>
+                </div>
+              </div>
+
+              <p className="mt-4 text-gray-700">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
+                sit rerum incidunt, a consequuntur recusandae ab saepe illo est
+                quia obcaecati neque quibusdam eius accusamus error officiis
+                atque voluptates magnam!
+              </p>
+            </blockquote>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -18,63 +18,63 @@ const Nav = () => {
   };
   return (
     <>
-      <div>
-        <div>
-          <div className="hidden md:flex  bg-orange-500 py-2 gap-2 md:gap-10 ">
-            <div className="flex flex-wrap ml-2">
-              <div className="flex ml-2 mb-2 md:mb-0 md:mx-10 cursor-pointer">
-                <HiOutlineMailOpen className="mt-1 text-lg" />
-                <h1 className="ml-1">info@seaindia.com</h1>
-              </div>
-              <div className="flex mb-2 ml-2 md:mb-0 cursor-pointer">
-                <MdPhone className="mt-1 text-lg" />
-                <Link to="tel:+917293420670">+91 7293420670</Link>
-              </div>
+      <div className="fixed top-0 left-0 w-full bg-white z-50">
+        <div className="hidden md:flex  bg-orange-500 py-2 gap-2 md:gap-10 ">
+          <div className="flex flex-wrap ml-2">
+            <div className="flex ml-2 mb-2 md:mb-0 md:mx-10 cursor-pointer">
+              <HiOutlineMailOpen className="mt-1 text-lg" />
+              <h1 className="ml-1">info@seaindia.com</h1>
             </div>
-            <div className="flex mr-2 mt-5 md:mt-1 ml-auto mr- md:mr-16 gap-8 text-lg">
-              <FaFacebookF />
-              <FaTwitter />
-              <RiInstagramFill />
+            <div className="flex mb-2 ml-2 md:mb-0 cursor-pointer">
+              <MdPhone className="mt-1 text-lg" />
+              <Link to="tel:+917293420670">+91 7293420670</Link>
             </div>
           </div>
-          <div className=" bg-white flex items-center justify-between">
-            <img
-              className=" ml-2 md:ml-9 mb-4 md:mb-0"
-              src={img}
-              alt="logo"
-              style={{ width: "130px", height: "65px" }}
-            />
-
-            <ul
-              className=" hidden md:flex ml-2  md:ml-auto md:gap-10 mr-4 md:mr-16 text-black font-semibold mt-2 md:mt-0 text-lg"
-              OnClick={closeNavbar}
-            >
-              <li className="hover:text-orange-500">
-                <Link to="/home">Home</Link>
-              </li>
-              <li className="hover:text-orange-500">
-                <Link to="/aboutus">About Us</Link>
-              </li>
-              <li className="hover:text-orange-500">
-                <Link to="/services">Services</Link>
-              </li>
-              <li className="hover:text-orange-500">
-                <Link to="/team">The Team</Link>
-              </li>
-              <li className="hover:text-orange-500">
-                <Link to="/contact">Contact Us</Link>
-              </li>
-            </ul>
-            <div className=" mr-2 lg:hidden md:flex flex-col justify-end ">
-              <button onClick={toggleNavbar}>
-                {mobileDrawerOpen ? <X /> : <Menu />}
-              </button>
-            </div>
+          <div className="flex mr-2 mt-5 md:mt-1 ml-auto mr- md:mr-16 gap-8 text-lg cursor-pointer">
+            <FaFacebookF />
+            <FaTwitter />
+            <RiInstagramFill />
           </div>
         </div>
+        <div className=" bg-white flex items-center justify-between">
+          <img
+            className=" ml-2 md:ml-9 mb-4 md:mb-0"
+            src={img}
+            alt="logo"
+            style={{ width: "130px", height: "65px" }}
+          />
 
+          <ul
+            className=" hidden md:flex ml-2  md:ml-auto md:gap-10 mr-4 md:mr-16 text-black font-semibold mt-2 md:mt-0 text-lg"
+            onClick={closeNavbar}
+          >
+            <li className="hover:text-orange-500">
+              <Link to="/home">Home</Link>
+            </li>
+            <li className="hover:text-orange-500">
+              <Link to="/aboutus">About Us</Link>
+            </li>
+            <li className="hover:text-orange-500">
+              <Link to="/services">Services</Link>
+            </li>
+            <li className="hover:text-orange-500">
+              <Link to="/team">The Team</Link>
+            </li>
+            <li className="hover:text-orange-500">
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className=" mr-2 lg:hidden md:flex flex-col justify-end ">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-24">
         {mobileDrawerOpen && (
-          <div className=" fixed top-20 left-0 z-20 text-xl font-bold text-black bg-orange-500 h-[370px] flex flex-col justify-center items-center w-[300px]  lg:hidden">
+          <div className="fixed top-20 text-white mt-4 right-0 z-20 text-xl font-bold text-black bg-orange-500 h-[450px] flex flex-col justify-center items-center w-[300px] transition-all duration-500 lg:hidden">
             <ul onClick={closeNavbar}>
               <li className="mt-10 ml-2">
                 <Link to="/home">Home</Link>
