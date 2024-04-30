cdcf9877e548bb743878a749b52612027e47af23
raghul1315 <raghuldharma02@gmail.com>
2024-04-30 11:36:15 +0530

    hh

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,6 +4,8 @@ import img15 from "../assets/image1.png";
 const About = () => {
   return (
     <section>
+      <br />
+      <br />
     <div className="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <div className="bg-orange-600 p-8 md:p-12 lg:px-16 lg:py-24">
