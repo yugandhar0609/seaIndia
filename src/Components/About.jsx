b4c45d22b0fd29197a1c89c279734772262760ee
raghul1315 <raghuldharma02@gmail.com>
2024-05-02 16:54:08 +0530

    dd

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section>
       <br />
-      <br/>
+      <br />
     <div className="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8 mt-5">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <div className="bg-orange-600 p-8 md:p-12 lg:px-16 lg:py-24">
